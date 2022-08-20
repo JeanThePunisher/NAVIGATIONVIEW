dd9d0bb54ad8c7dbf2173a2a0bdcc78889b91c77
TheJean <101907265+JeanThePunisher@users.noreply.github.com>
2022-08-20 12:48:20 -0500

    Actualización tarea JSON

--- app/src/main/java/com/example/navigationview/MainActivity.kt
+++ app/src/main/java/com/example/navigationview/MainActivity.kt
@@ -6,40 +6,57 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import com.android.volley.*
+import com.android.volley.toolbox.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity(){
+
+    val url = "https://my-json-server.typicode.com/JeanThePunisher/ArchivosJson/usuarios"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.setDisplayShowHomeEnabled(true)
+
+
     }
     fun enviarDatos(view: View){
+        val queue = Volley.newRequestQueue(this)
         val intent= Intent(this,NavigationView::class.java)
-        val usertxt=findViewById<EditText>(R.id.usertxt)
-        val passtxt=findViewById<EditText>(R.id.passtxt)
+        val data=findViewById<EditText>(R.id.usertxt)
+        val pass=findViewById<EditText>(R.id.passtxt)
 
-        if(usertxt.text.toString()=="Jeandavid"&& passtxt.text.toString()=="admin")
-        {
-            intent.putExtra("usuario",usertxt.text.toString())
-            intent.putExtra("password",passtxt.text.toString())
-            intent.putExtra("rol","Administrador")
-            startActivity(intent)
-        }
-        else if(usertxt.text.toString()=="JoseM"&& passtxt.text.toString()=="user")
-        {
-            intent.putExtra("usuario",usertxt.text.toString())
-            intent.putExtra("password",passtxt.text.toString())
-            intent.putExtra("rol","user")
-            startActivity(intent)
-        }
-        else
-        {
-            Toast.makeText(this,"Usuario o contraseña son incorrectos", Toast.LENGTH_SHORT).show()
-        }
+        val request = StringRequest(
+            Request.Method.GET, url,
+            { response ->
+                val users = JSONArray(response.toString());
+                for(i in 0 until users.length())
+                {
+                    val user: JSONObject = users.getJSONObject(i);
+                    if(data.text.toString() == user.getString("usuario"))
+                    {
+                        if (pass.text.toString() == user.getString("contraseña"))
+                        {
+                            intent.putExtra("id",user.getString("id"))
+                            intent.putExtra("usuario",user.getString("usuario"))
+                            intent.putExtra("contraseña",user.getString("contraseña"))
+                            intent.putExtra("rol",user.getString("rol"))
+                            startActivity(intent)
+                        }
+
+                    }
+                }
+            },
+            {
+                Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT).show()
+            })
+        queue.add(request)
 
     }
 
-
 }
+
--- app/src/main/java/com/example/navigationview/NavigationView.kt
+++ app/src/main/java/com/example/navigationview/NavigationView.kt
@@ -10,71 +10,99 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.navigation.NavigationView
+import org.json.JSONArray
+import org.json.JSONObject
+
 
 class NavigationView : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
     lateinit var drawerlayout: DrawerLayout
-    lateinit var toggle:ActionBarDrawerToggle
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navegacionview)
+        val url = "https://my-json-server.typicode.com/JeanThePunisher/archivo-json-2/items"
+        val queue = Volley.newRequestQueue(this)
+        val bundle = intent.extras
+        var toolbar: Toolbar = findViewById(R.id.toolbar_main)
 
-        val bundle=intent.extras
-        var toolbar: Toolbar =findViewById(R.id.toolbar_main)
         setSupportActionBar(toolbar)
-        drawerlayout=findViewById(R.id.drawer_layout)
-        toggle= ActionBarDrawerToggle(this,drawerlayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close)
+        drawerlayout = findViewById(R.id.drawer_layout)
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawerlayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
         drawerlayout.addDrawerListener(toggle)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
-        var navigationView: NavigationView =findViewById(R.id.nav_view)
+        var navigationView: NavigationView = findViewById(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this)
-        var headerView: View=navigationView.getHeaderView(0)
-        var userName:TextView=headerView.findViewById(R.id.user_name)
+        var headerView: View = navigationView.getHeaderView(0)
+        var userName: TextView = headerView.findViewById(R.id.user_name)
         userName.setText(bundle?.getString("usuario"))
-        var imageView:ImageView=headerView.findViewById(R.id.image_view)
-        var menu:Menu=navigationView.menu
+        var imageView: ImageView = navigationView.getHeaderView(0).findViewById(R.id.image_view)
+        var menu: Menu = navigationView.menu
 
-        if(bundle?.getString("rol")=="Administrador"){
+
+        if (bundle?.getString("id") != null) {
+            Toast.makeText(
+                this,
+                "Bienvenido " + bundle?.getString("rol") +" "+ bundle?.getString("usuario"),
+                Toast.LENGTH_SHORT
+            ).show()
             imageView.setImageResource(R.drawable.admin)
-            menu.add("Red")
-            menu.add("Estadisticas")
-            menu.add("Contrato")
-            menu.add("Configuración")
-            menu.add("Salir")
-        }else if (bundle?.getString("rol")=="user"){
-            imageView.setImageResource(R.drawable.user)
-            menu.add("Perfil")
-            menu.add("Calificaciones")
-            menu.add("Libros")
-            menu.add("Configuración")
-            menu.add("Salir")
+            val request = StringRequest(
+                Request.Method.GET, url,
+                { response ->
+                    val ItemsMenu = JSONArray(response.toString());
+                    for (i in 0 until ItemsMenu.length()) {
+                        val items: JSONObject = ItemsMenu.getJSONObject(i)
+                        if (bundle?.getString("id") == "1" && items.getString("id") == "1") {
+                            menu.add(items.getString("item1"))
+                            menu.add(items.getString("item2"))
+                            menu.add(items.getString("item3"))
+                            menu.add(items.getString("item4"))
+                            menu.add(items.getString("item5"))
+                            menu.add(items.getString("item6"))
+                        } else if (bundle?.getString("id") == "2" && items.getString("id") == "2") {
+                            menu.add(items.getString("item1"))
+                            menu.add(items.getString("item2"))
+                            menu.add(items.getString("item3"))
+                            menu.add(items.getString("item4"))
+                            imageView.setImageResource((R.drawable.user))
+                        } else if (bundle?.getString("id") == "3" && items.getString("id") == "3") {
+                            menu.add(items.getString("item1"))
+                            menu.add(items.getString("item2"))
+                            menu.add(items.getString("item3"))
+                            imageView.setImageResource((R.drawable.maes))
+                        } else if (bundle?.getString("id") == "4" && items.getString("id") == "4") {
+                            menu.add(items.getString("item1"))
+                            menu.add(items.getString("item2"))
+                            menu.add(items.getString("item3"))
+                            menu.add(items.getString("item4"))
+                            menu.add(items.getString("item5"))
+                            imageView.setImageResource((R.drawable.emple))
+                        }
+                    }
+                },
+                {
+                    Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT).show()
+                })
+            queue.add(request)
+
+
         }
     }
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        var fragment: Fragment? = null
-        when(item.title){
-
-            "Estadisticas"-> {
-                fragment= DatosUser()
-                Toast.makeText(this,"Estadísticas", Toast.LENGTH_SHORT).show()
-            }
-        }
-        if (fragment != null) {
-            getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.content_frame, fragment)
-                .commit()
-
-            item.setChecked(true)
-            getSupportActionBar()?.setTitle(item.getTitle());
-        }
-        drawerlayout.closeDrawer(GravityCompat.START)
-        return true
+        TODO("Not yet implemented")
     }
 }
